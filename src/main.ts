837ac0ca01e5cfcf0427b789b2a59a79d231ce5f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2025-11-03 23:09:23 -0500

    feat: Implement DownloadSongService for downloading songs from YouTube Music

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello, YT Music Downloader!");
+import { DownloadSongService } from "./application/DownloadSongService.js";
+import { YoutubeClient } from "./infrastructure/youtube/YoutubeClient.js";
+import { YoutubeMusicDownloader } from "./infrastructure/youtube/YoutubeMusicDownloader.js";
+
+const client = await YoutubeClient.initializeAndCreate();
+const downloader = new YoutubeMusicDownloader(client);
+const service = new DownloadSongService(downloader);
+
+service
+  .execute("https://music.youtube.com/watch?v=4NfN-bdwfLA")
+  .then((song) => console.log("Descargado:", song));
