a0c70cc5099578080a942f0ba9fe85f83bfed16f
Carlos Muñoz <munozrengifocarlos@gmail.com>
2025-11-04 22:51:40 -0500

    feat: Refactor main.ts to initialize FFmpegConverter, FileSystemManager, and MetadataWriter for enhanced downloader functionality

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { DownloadSongService } from "./application/DownloadSongService.js";
+import { FFmpegConverter } from "./infrastructure/filesystem/FFmpegConverter.js";
+import { FileSystemManager } from "./infrastructure/filesystem/FileSystemManager.js";
+import { MetadataWriter } from "./infrastructure/filesystem/MetadataWriter.js";
 import { YoutubeClient } from "./infrastructure/youtube/YoutubeClient.js";
 import { YoutubeMusicDownloader } from "./infrastructure/youtube/YoutubeMusicDownloader.js";
 
 const client = await YoutubeClient.initializeAndCreate();
-const downloader = new YoutubeMusicDownloader(client);
+const converter = new FFmpegConverter();
+const fileSystem = new FileSystemManager();
+const metadataWriter = new MetadataWriter();
+
+const downloader = new YoutubeMusicDownloader(
+  client,
+  fileSystem,
+  converter,
+  metadataWriter
+);
+
 const service = new DownloadSongService(downloader);
 
 service
-  .execute("https://music.youtube.com/watch?v=4NfN-bdwfLA")
+  .execute("https://music.youtube.com/watch?v=8NdXe_EmNhY")
   .then((song) => console.log("Descargado:", song));
